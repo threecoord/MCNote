a8b963b6b6506fc020740abbe97a0ea6ab76ceca
threecoord <223322590+threecoord@users.noreply.github.com>
2025-08-29 04:08:31 +0800

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,37 @@
+﻿
+async function Main() {
+        document.body.style.backgroundColor = "black";
+        let sNote = "";
+        await new Promise((resolve) => {
+                const xhr = new XMLHttpRequest();
+                xhr.open("get", "./Note.txt", true);
+                xhr.send();
+                xhr.onreadystatechange = () => {
+                        if (xhr.readyState == 4 && xhr.status == 200) {
+                                sNote = xhr.responseText;
+                                resolve();
+                        }
+                };
+        });
+        const asLine = sNote.split('\r\n');
+        const nWidthBody = document.body.clientWidth;
+        const nWidthWord = nWidthBody / 10;
+        const nHeightLine = nWidthWord / 2;
+        asLine.forEach(sLine => {
+                if (sLine.trim()) {
+                        const [sState, sTask] = sLine.split('\t');
+                        const eP = document.createElement('p');
+                        eP.textContent = sTask;
+                        eP.style.fontSize = `${nWidthWord}px`;
+                        eP.style.lineHeight = `${nHeightLine}px`;
+                        if(sState == "0") {
+                                eP.style.color = "#ffff00";
+                        } else {
+                                eP.style.color = "#00ff00";
+                        }
+                        document.body.appendChild(eP);
+                }
+        });
+}
+
+await Main();
